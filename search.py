d2c8a8dda83d0ada081bc9833af599849a4868bc
Morgan Jensen <morganjensen2@mail.weber.edu>
2022-04-12 14:43:52 -0600

    starting

---
+++ search.py
@@ -0,0 +1,18 @@
+import tweepy
+import config
+
+client = tweepy.Client(bearer_token=config.BEARER_TOKEN)
+
+query = '#POTUS -is:retweet'
+
+response = client.search_recent_tweets(query=query, max_results=10)
+
+for tweet in response.data:
+    print(tweet.text + '\n')
+
+# Todo:
+#   * save tweets in some sort of fashion
+#   * feed tweets into Microsoft AI
+#   * save output in some sort of fashion
+#   * use pandas to graph changes
+
