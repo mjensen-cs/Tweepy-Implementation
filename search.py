35388cf43d1e46c180fdf6688462ed42d09991cc
Morgan Jensen <morganjensen2@mail.weber.edu>
2022-04-13 11:01:07 -0600

    added english filter to query

--- search.py
+++ search.py
@@ -3,16 +3,17 @@ import config
 
 client = tweepy.Client(bearer_token=config.BEARER_TOKEN)
 
-query = '#POTUS -is:retweet'
+query = '#POTUS lang:en -is:retweet'
 
 response = client.search_recent_tweets(query=query, max_results=10)
 
 for tweet in response.data:
     print(tweet.text + '\n')
 
+# response.data is JSON
+
 # Todo:
 #   * save tweets in some sort of fashion
 #   * feed tweets into Microsoft AI
 #   * save output in some sort of fashion
 #   * use pandas to graph changes
-
